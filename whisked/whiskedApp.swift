e7130162ae9b97d66ad4e82c011d2ebc588e8de1
Luke Skinner <lukeskinner101@gmail.com>
2026-02-01 13:25:55 -0700

    add files

---
+++ whisked/whiskedApp.swift
@@ -0,0 +1,17 @@
+//
+//  whiskedApp.swift
+//  whisked
+//
+//  Created by Luke Skinner on 2/1/26.
+//
+
+import SwiftUI
+
+@main
+struct whiskedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
