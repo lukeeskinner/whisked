b19794c4df4250a7fb2659212e61a21635455e73
Luke Skinner <lukeskinner101@gmail.com>
2026-02-10 23:24:38 -0700

    changing some text and added basic style

---
+++ whisked/Views/HomeScreen/HomeScreen.swift
@@ -0,0 +1,18 @@
+import SwiftUI
+//
+//  HomeScreen.swift
+//  whisked
+//
+//  Created by Luke Skinner on 2/1/26.
+//
+
+struct HomeScreen: View {
+    var body: some View {
+        
+    }
+    
+    
+}
+
+
+
